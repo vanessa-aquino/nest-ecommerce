532118915b31acd18175f0bd1d08b1c3bc05a514
Vanessa <vanessaquinoo@hotmail.com>
2025-01-29 17:11:51 -0300

    style: finalizado o component nav e header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
