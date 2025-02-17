5714979781bde93e8a527af9d0a1d0e46e13ff8d
Vanessa <vanessaquinoo@hotmail.com>
2025-02-17 17:43:51 -0300

    feat: atualizando as dependencias angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
+import { MatDialogModule } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions())
+    provideRouter(routes, withViewTransitions()),
+    importProvidersFrom(MatDialogModule)
   ]
 };
